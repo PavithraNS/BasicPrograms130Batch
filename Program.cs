2c09c76c8466760aa819d2b35fef009f4d652983
PavithraNS <nspavi1991@gmail.com>
2022-03-17 11:29:59 +0530

    [Pavithra] Modify Program.cs

--- Program.cs
+++ Program.cs
@@ -13,6 +13,7 @@ namespace HelloProject130Batch
             Console.WriteLine("Welcome to 130batch");
             Console.WriteLine("----------");
             Console.WriteLine("Remote changing modified by 2nd developer");
+            Console.WriteLine("Local changes creating conflict");
             Console.ReadLine();
         }
     }
