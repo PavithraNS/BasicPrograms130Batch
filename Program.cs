5ceb0d3390e42118af23a8e41f6bf0676ae0206f
PavithraNS <nspavi1991@gmail.com>
2022-03-19 11:16:14 +0530

    [Pavithra] Add pushing local branch Fibonoccie

--- Program.cs
+++ Program.cs
@@ -11,16 +11,26 @@ namespace HelloProject130Batch
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to 130batch");
-            Console.WriteLine("----------");
-            Console.WriteLine("Remote changing modified by 2nd developer");
-            Console.WriteLine("Local changes creating conflict");
-            Console.WriteLine("Creating conflict from remote");
-            Human human = new Human();
-            human.HumanDetails();
-            Human.Speak();
-            SwapTwoNumbers swapTwoNumbers = new SwapTwoNumbers();
-            swapTwoNumbers.SwapNumbers();
+            //Console.WriteLine("Welcome to 130batch");
+            //Console.WriteLine("----------");
+            //Console.WriteLine("Remote changing modified by 2nd developer");
+            //Console.WriteLine("Local changes creating conflict");
+            //Console.WriteLine("Creating conflict from remote");
+            //Human human = new Human();
+            //human.HumanDetails();
+            //Human.Speak();
+            SwapTwoNumbers swap = new SwapTwoNumbers();
+            swap.first = 10;
+            SwapTwoNumbers swapTwoNumbers2 = new SwapTwoNumbers();
+            swapTwoNumbers2.first=30;
+            SwapTwoNumbers.third = 40;
+            Console.WriteLine("first object:"+ swap.first+ " static " +SwapTwoNumbers.third);
+            Console.WriteLine("second object:" + swapTwoNumbers2.first+ " static " + SwapTwoNumbers.third);
+            Console.WriteLine("after modifying static variable");
+            SwapTwoNumbers.third = 56;
+            Console.WriteLine("first object:" + swap.first + " static " + SwapTwoNumbers.third);
+            Console.WriteLine("second object:" + swapTwoNumbers2.first + " static " + SwapTwoNumbers.third);
+            //Console.WriteLine("static varibel:"+ SwapTwoNumbers.third);
             Console.ReadLine();
         }
     }
